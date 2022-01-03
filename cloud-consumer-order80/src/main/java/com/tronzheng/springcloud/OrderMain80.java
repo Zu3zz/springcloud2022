a81dc8f574d43518956e7be1e5ff914aab1e10e4
Zu3zz <zthzththth@163.com>
2022-01-03 18:47:33 +0800

    feat: add order main project init

---
+++ cloud-consumer-order80/src/main/java/com/tronzheng/springcloud/OrderMain80.java
@@ -0,0 +1,17 @@
+package com.tronzheng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 3zZ.
+ * @date 2022/1/3.
+ */
+
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
